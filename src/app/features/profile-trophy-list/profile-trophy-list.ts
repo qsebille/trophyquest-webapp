23c89f267fc6a84c3d93d934b101104e94f0b15f
Quentin Sebille <quentin.sebille@gmail.com>
2025-11-25 13:42:03 +0100

    Improved trophy list display by updating icon handling, adding trophy type size property, and refining layout elements.

--- src/app/features/profile-trophy-list/profile-trophy-list.ts
+++ src/app/features/profile-trophy-list/profile-trophy-list.ts
@@ -2,11 +2,13 @@ import {Component, Input} from '@angular/core';
 import {EarnedTrophy} from '../../core/models/dto/earned-trophy';
 import {MatCardModule} from '@angular/material/card';
 import {DatePipe, NgOptimizedImage} from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-profile-trophy-list',
   imports: [
     MatCardModule,
+    MatIconModule,
     NgOptimizedImage,
     DatePipe,
   ],
@@ -16,4 +18,5 @@ import {DatePipe, NgOptimizedImage} from '@angular/common';
 export class ProfileTrophyList {
   @Input({required: true}) trophyList: EarnedTrophy[] = [];
   readonly iconSize: number = 50
+  readonly trophyTypeSize: number = 50
 }
