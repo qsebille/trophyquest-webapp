a62e22fcef46223b2b368e796518308b0f378784
qsebille <quentin.sebille@gmail.com>
2025-11-27 10:02:53 +0100

    Refactored user game navigation logic (smart/dumb) (#10)

--- src/app/pages/profile-page/profile-page.spec.ts
+++ src/app/pages/profile-page/profile-page.spec.ts
@@ -1,18 +1,20 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ProfilePage} from './profile-page';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {ProfileStore} from '../../core/store/profile-store';
 import {Component, Input} from '@angular/core';
-import {UserGame} from '../../core/models/dto/user-game';
 import {TrophyCount} from '../../core/models/dto/trophy-count';
 import {EarnedTrophy} from '../../core/models/dto/earned-trophy';
 import {User} from '../../core/models/dto/user';
+import {ProfileGameList} from '../../components/profile-game-list/profile-game-list';
+import {UserGame} from '../../core/models/dto/user-game';
 
 describe('ProfilePage', () => {
   let component: ProfilePage;
   let fixture: ComponentFixture<ProfilePage>;
   let profileStoreSpy: jasmine.SpyObj<ProfileStore>;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   @Component({
     selector: 'app-profile-summary',
@@ -21,14 +23,6 @@ describe('ProfilePage', () => {
   class MockProfileSummary {
     @Input({required: true}) profile: User = undefined!;
     @Input({required: true}) trophyCount: TrophyCount = {platinum: 0, gold: 0, silver: 0, bronze: 0};
-  }
-
-  @Component({
-    selector: 'app-profile-game-list',
-    template: ''
-  })
-  class MockProfileGameList {
-    @Input({required: true}) gameList: UserGame[] = [];
   }
 
   @Component({
@@ -42,18 +36,21 @@ describe('ProfilePage', () => {
   const userProfileId = '1';
 
   beforeEach(async () => {
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
     profileStoreSpy = jasmine.createSpyObj('ProfileStore', ['fetch', 'user', 'trophyCount', 'gameList', 'trophyList']);
+
     await TestBed.configureTestingModule({
-      imports: [ProfilePage, MockProfileSummary, MockProfileGameList, MockProfileTrophyList],
+      imports: [ProfilePage, MockProfileSummary, ProfileGameList, MockProfileTrophyList],
       providers: [
         {provide: ProfileStore, useValue: profileStoreSpy},
+        {provide: Router, useValue: routerSpy},
         {provide: ActivatedRoute, useValue: {snapshot: {paramMap: {get: () => userProfileId}}}}
       ]
     })
       .compileComponents();
 
     TestBed.overrideComponent(ProfilePage, {
-      set: {imports: [MockProfileSummary, MockProfileGameList, MockProfileTrophyList]}
+      set: {imports: [MockProfileSummary, ProfileGameList, MockProfileTrophyList]}
     });
 
     fixture = TestBed.createComponent(ProfilePage);
@@ -67,6 +64,24 @@ describe('ProfilePage', () => {
 
   it('should fetch profile on init', () => {
     expect(profileStoreSpy.fetch).toHaveBeenCalledWith(userProfileId);
+  });
+
+  it('should navigate to game page when clicking on game card', () => {
+    const mockedTrophyCount: TrophyCount = {platinum: 1, gold: 2, silver: 3, bronze: 4};
+    const game: UserGame = {
+      id: '123',
+      title: 'Game 1',
+      imageUrl: 'game.png',
+      earnedTrophies: mockedTrophyCount,
+      totalTrophies: mockedTrophyCount,
+      trophyCollections: []
+    };
+    routerSpy.navigate.and.returnValue(Promise.resolve(true));
+
+    component.navigateToGamePage(game);
+
+
+    expect(routerSpy.navigate).toHaveBeenCalledOnceWith(['/game', game.id]);
   });
 
 });
--- src/app/pages/profile-page/profile-page.ts
+++ src/app/pages/profile-page/profile-page.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {ProfileStore} from '../../core/store/profile-store';
 import {ProfileSummary} from '../../components/profile-summary/profile-summary';
 import {ProfileGameList} from '../../components/profile-game-list/profile-game-list';
 import {ProfileTrophyList} from '../../components/profile-trophy-list/profile-trophy-list';
+import {UserGame} from '../../core/models/dto/user-game';
 
 @Component({
   selector: 'app-profile-page',
@@ -18,7 +19,16 @@ import {ProfileTrophyList} from '../../components/profile-trophy-list/profile-tr
 export class ProfilePage {
   userProfileId!: string | null;
 
-  constructor(private readonly _route: ActivatedRoute, public readonly profileStore: ProfileStore) {
+  constructor(
+    private readonly _router: Router,
+    private readonly _route: ActivatedRoute,
+    public readonly profileStore: ProfileStore,
+  ) {
+  }
+
+  navigateToGamePage(game: UserGame) {
+    const gameId = game.id;
+    this._router.navigate(['/game', gameId]).then(() => console.info(`Navigated to game page: ${gameId}`));
   }
 
   ngOnInit(): void {
