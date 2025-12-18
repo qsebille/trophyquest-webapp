46389d19d9919ad5162eb43a4dad4bcfd513c205
qsebille <quentin.sebille@gmail.com>
2025-12-18 13:27:39 +0100

    Fix deploy workflow (#28)
    * Fix deploy workflow
    
    * Fix http services url

--- src/app/core/services/http/collection.service.ts
+++ src/app/core/services/http/collection.service.ts
@@ -8,7 +8,7 @@ import {environment} from "../../../../environments/environment";
     providedIn: 'root',
 })
 export class CollectionService {
-    private readonly API_URL = `${environment.apiUrl}/api/game`;
+    private readonly API_URL = `${environment.apiUrl}/api/collection`;
 
     constructor(private _http: HttpClient) {
     }
